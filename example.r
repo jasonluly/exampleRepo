0afbe8a9ddc106897997890c55446f09c85710d0
jasonluly <jasonluly@gmail.com>
2014-05-30 14:09:16 -0400

    not sure

--- example.r
+++ example.r
@@ -13,6 +13,8 @@ d <- data.frame(`id`    = as.character(seq(1, 1000)),
                 `site`  = sample(c('AAA', 'BBB', 'CCC', 'DDD'), size=1000, 
                                  replace=TRUE),
                 `age`   = rnorm(n=1000, mean=50,  sd=5),
+                `sex`   = sample(c('Male', 'Female'), size=1000,
+                                 replace=TRUE),
                 `height`= rnorm(n=1000, mean=72,  sd=10),
                 `weight`= rnorm(n=1000, mean=150, sd=10))
 
@@ -25,9 +27,18 @@ table1 <- ddply(d, .(site), summarise,
                   height=tempFunction(height),
                   weight=tempFunction(weight))
 
-# plot data
+# plot data - age
 p <- ggplot(data=d)
 p <- p + geom_histogram(aes(x=age), binwidth=1)
 p <- p + facet_grid(site ~ .)
 print(p)
+
+# plot data - sex
+p <- ggplot(data=d)
+p <- p + geom_bar(aes(x=sex))
+p <- p + facet_grid(site ~ .)
+print(p)
+
+
+
 
